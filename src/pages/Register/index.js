7e82fae7d7bb62bc79e4d30b8b4f8563264290e9
Zanma <zainurimahmud32@gmail.com>
2022-02-03 14:52:22 +0700

    slicing register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
+import "./register.scss";
 
 function Register() {
   return (
-    <div>
-      <p>Register page</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} alt="register background" className="bg-image" />
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full Name" placeholder="Full Name" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" />
+        <Gap height={100} />
+        <Link title="Kembali ke Login" />
+      </div>
     </div>
   );
 }
